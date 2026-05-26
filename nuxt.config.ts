12041913daad5e975ea1377b6bcf078d9752a590
Thyago Euclides <flooddk@gmail.com>
2026-05-26 15:39:49 -0400

    Init project

---
+++ nuxt.config.ts
@@ -0,0 +1,56 @@
+import tailwindcss from "@tailwindcss/vite";
+
+import { repositoryName } from "./slicemachine.config.json";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: "Prismic + Nuxt Minimal Starter",
+      htmlAttrs: { lang: "en" },
+      meta: [
+        { charset: "utf-8" },
+        { name: "apple-mobile-web-app-title", content: "Farbe" },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-iconddd",
+          href: "/apple-touch-icon.png",
+          sizes: "180x180",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon-96x96.png",
+          sizes: "96x96",
+        },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+    },
+  },
+
+  css: ["~/assets/css/index.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  modules: ["@nuxt/eslint", "@nuxtjs/prismic", "@nuxt/fonts"],
+
+  prismic: {
+    endpoint: repositoryName,
+    preview: "/api/preview",
+    clientConfig: {
+      routes: [
+        { type: "page", uid: "home", path: "/" },
+        { type: "page", path: "/:uid" },
+      ],
+    },
+  },
+
+  compatibilityDate: "2026-01-02",
+});
