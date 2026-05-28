fa6a39395dc8c4f9a2c2afa5dc202c7b0319676e
Thyago Euclides <flooddk@gmail.com>
2026-05-28 15:08:59 -0400

    Add `TCanvas` and `TScene` components with Three.js integration
    Introduce `@tresjs/cientos` and related dependencies for 3D rendering. Update `default.vue` to incorporate `TCanvas` for enhanced visual effects.

---
+++ app/components/TCanvas.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { OrbitControls } from "@tresjs/cientos";
+import { TresCanvas } from "@tresjs/core";
+
+const mounted = ref(false);
+
+onMounted(() => {
+  mounted.value = true;
+});
+</script>
+
+<template>
+  <figure
+    class="transition-opacity delay-300 duration-1000 ease-in-out"
+    :class="{ 'opacity-0': !mounted }"
+  >
+    <TresCanvas>
+      <OrbitControls />
+      <TresPerspectiveCamera :fov="45" :position="[0, 0, 20]" />
+      <TresMesh>
+        <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+        <TresMeshBasicMaterial color="orange" />
+      </TresMesh>
+    </TresCanvas>
+  </figure>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 import { asImageSrc } from "@prismicio/client";
 import { useCart } from "~/composables/useCart";
+import TScene from "~/components/TScene.vue";
+
 
 const route = useRoute();
 const { client } = usePrismic();
@@ -29,7 +31,7 @@ onMounted(() => {
     <AppHeader :settings="settings" class="fixed top-0 left-0 right-0 z-10" />
     <slot />
     <AppFooter :settings="settings" />
-    <TCanvas class="fixed top-0 left-0 right-0 h-lvh -z-1">
+    <TCanvas class="fixed top-0 left-0 right-0 h-lvh ">
       <TScene />
     </TCanvas>
   </div>
